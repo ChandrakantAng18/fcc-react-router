49dd9091f86398bfbf79be050eb3d2fedbd93d5a
ChandrakantAng18 <mailatchandrakant@gmail.com>
2025-05-22 22:15:02 +0530

    temp code for router

---
+++ src/components/AboutUs.js
@@ -0,0 +1,9 @@
+const AboutUs = () => {
+  return (
+    <div>
+      <h1>This is a ABOUT US Page</h1>
+    </div>
+  );
+};
+
+export default AboutUs;
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>This is a HOME component</h1>
+    </div>
+  );
+};
+
+export default Home;
